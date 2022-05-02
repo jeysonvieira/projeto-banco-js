2521f0c2466bab1d3aa2cfe243c534d3ce970087
Jeyson vieira <98441884+jeysonvieira@users.noreply.github.com>
2022-05-02 03:45:37 -0300

    Update do projeto
    Mais uma parte do projeto

--- banco.js
+++ banco.js
@@ -6,8 +6,11 @@ var input = require("readline-sync")
 
 const validacao = (nome) => {
     for(let cont = 0; cont < cadastrados.length; cont++){
+        var contador = cont
         if(cadastrados[cont].nome == nome){
-            return true;
+
+            return true
+            
         }
 
     }
@@ -15,9 +18,53 @@ const validacao = (nome) => {
 }
 
 
-//const validacao_resto = (dados) => {
+const id = (nome_id) => {
+    var contador = 0
+    while(contador < cadastrados.length){
+        if(cadastrados[contador].nome == nome_id){
+            return contador
+        }
+    contador += 1
+    }
+}
 
-// }
+
+const cartao_senha = (cartao, senha) =>{
+    if(cartao == cadastrados[id_usuario].cartao && senha == cadastrados[id_usuario].senha){
+        return true
+    }
+}
+
+function tabela(){
+    return `[1] - DEPÓSITAR
+[2] - SACAR
+[3] - SALDO
+[4] - DADOS DA CONTA
+[5] - SAIR`
+}
+
+
+function depositar(valor){
+    novo_saldo = cadastrados[id_usuario].saldo + valor;
+    return novo_saldo
+}
+
+function sacar(valor){
+    novo_saldo = cadastrados[id_usuario].saldo - valor;
+    return novo_saldo
+}
+
+function saldo(){
+    saldo_inicial = cadastrados[id_usuario].saldo
+    return saldo_inicial
+}
+
+function dados(){
+    return `Nome: ${cadastrados[id_usuario].nome}
+Cpf: ${cadastrados[id_usuario].cpf}
+Cartao: ${cadastrados[id_usuario].cartao}
+Saldo: ${cadastrados[id_usuario].saldo}`
+}
 
 
 
@@ -76,16 +123,19 @@ const cadastrados = [
 if(resp === 'S'){
     let nome_usuario = input.question("Digite seu nome completo: ").toUpperCase().trim();
 
-    let conferir = validacao(nome_usuario);
-
 
     // VALIDAÇÃO DO NOME DIGITADO PELO CLIENTE //
 
 
-    if(conferir){
-        console.log(`Seja Bem vindo Sr(a): ${nome_usuario.toLocaleLowerCase()}.`);
+    let conferir = validacao(nome_usuario);
 
-        var usuario_validado = nome_usuario.toLowerCase();
+
+    if(conferir){
+        console.log(`Seja Bem vindo(a) Sr(a): ${nome_usuario.toLocaleLowerCase()}.`);
+
+        var usuario_validado = nome_usuario;
+
+        var id_usuario = id(usuario_validado)
 
     }
     else{
@@ -100,21 +150,97 @@ Nome não encontrado no sistema, Digite corretamente.
             if(conferir){
                 console.log(`Seja Bem vindo Sr(a): ${nome_usuario.toLocaleLowerCase()}.`)
 
-                var usuario_validado = nome_usuario.toLowerCase();
+                usuario_validado = nome_usuario;
+
+                id_usuario = id(usuario_validado)
 
                 break
             }
     }
+}
     
     
     // VALIDAÇÃO DO RESTO DAS INFORMAÇÕES //
 
-    
+
+
+console.log(`
+Agora precisamos do resto do seus dados para acessar sua conta no banco.
+`);
+
+
+//let cartao_usuario = input.question(`Digite seu número do cartão: `);
+
+//let senha_usuario = input.question(`Digite sua senha: `);
+
+
+//var dados = cartao_senha(cartao_usuario, senha_usuario);
+
+if(true){
+    console.log(tabela())
+
+    let menu = Number(input.question(`
+O que deseja fazer?: `))
+
+    switch(menu){  
+        case 1:
+
+            console.log(`Seu saldo é de: ${saldo()}`)
+
+            var deposito = Number(input.question(`Qual valor deseja depósitar?: `))
+
+            var deposito_usuario = depositar(deposito)
+
+            console.log(`Saldo atual: ${deposito_usuario}`)
+
+            break;
+        
+        case 2:
+
+            console.log(`Seu saldo é de: ${saldo()}`)
+
+            var saque = Number(input.question(`Qual valor deseja sacar?: `))
+
+            var saque_usuario = sacar(saque)
+
+            console.log(`Saldo atual: ${saque_usuario}`)
+
+            break;
+
+        case 3:
+            console.log(`Seu saldo é : ${saldo()}`)
+            break;
+
+
+        case 4:
+            console.log(dados())
+            break;
+
+
+        case 5:
+            break;
+
+
+        default:
+            console.log('Opção não encontrada.')
+            
+
+    }
 }
+
+else{
+
 }
 
+}
 
+else if(resp == 'N'){
+    console.log(`Vamos criar um cadastro para você.`)
 
+    nome = input.question(`Digite seu nome: `);
 
+    (cadastrados[cadastrados[3]] = {nome: nome})
 
-
+    console.log(cadastrados)
+        
+    }
